4f2faf6964094c1c58554b1c9be435742b815d69
Upendra5791 <upenz5791@gmail.com>
2020-05-14 21:23:23 +0530

    UC : fixed error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express'),
 // app.set('view engine', 'ejs')
 app.engine('html', require('ejs').renderFile);    
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
